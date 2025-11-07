ebd58424907ac44d382f1dcd20397d1bd2a48fcb
Greedysky <greedysky@163.com>
2025-11-07 15:15:54 +0800

    feat: Support for qmmp 0.x Qt4

--- effectsonicfactory.cpp
+++ effectsonicfactory.cpp
@@ -46,3 +46,8 @@ QString EffectSonicFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(sonic, EffectSonicFactory)
+#endif
